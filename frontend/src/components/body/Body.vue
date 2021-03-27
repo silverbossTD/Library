7bf671dcb3c41ab0b93d86cd85391240bbfc6989
silverbosstd <sologame37@gmail.com>
2021-03-27 14:38:09 +0700

    Add preloader / complete books page

--- frontend/src/components/body/Body.vue
+++ frontend/src/components/body/Body.vue
@@ -1,45 +1,33 @@
 <template>
     <div class="body container">
-        <div id="list">
+        <div class="page-loader2" v-if="!books">
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+        </div>
+        <div id="list" v-if="books">
             <hr />
             <h1 class="p-3 text-center">Trending Books</h1>
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
                     <ul class="list-card">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 18rem;"
+                            v-for="(book, index) in books.slice(0, 3)"
+                            v-bind:item="book"
+                            v-bind:index="index"
+                            v-bind:key="book.id"
+                        >
                           <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
+                              <img class="card-img-top"
+                                :src="book.image"
+                                alt="Card image cap"
+                                style="height: 266.66px"
+                              >
                               <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
+                                <h3 class="card-title">{{ book.title }}</h3>
+                                <p class="card-date">{{ book.date }}</p>
                               </div>
                           </a>
                         </div>
@@ -47,79 +35,21 @@
                 </div>
                 <div class="carousel-item">
                     <ul class="list-card">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 18rem;"
+                            v-for="(book, index) in books.slice(3, 6)"
+                            v-bind:item="book"
+                            v-bind:index="index"
+                            v-bind:key="book.id"
+                        >
                           <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
+                              <img class="card-img-top"
+                                :src="book.image"
+                                alt="Card image cap"
+                                style="height: 266.66px"
+                              >
                               <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                    </ul>
-                </div>
-                <div class="carousel-item">
-                    <ul class="list-card">
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
-                              </div>
-                          </a>
-                        </div>
-                        <div class="card" style="width: 18rem;">
-                          <a href="#">
-                              <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/1071-1/AF8/1B4/E9/%7BAF81B4E9-C23C-40C7-8FB5-2D9F9FBAE645%7DImg400.jpg" alt="Card image cap">
-                              <div class="card-body">
-                                <h3 class="card-title">Card title</h3>
-                                <p class="card-date">20-9-2021</p>
+                                <h3 class="card-title">{{ book.title }}</h3>
+                                <p class="card-date">{{ book.date }}</p>
                               </div>
                           </a>
                         </div>
@@ -140,39 +70,21 @@
 
             <h1 class="p-3 text-center">Recommended Reads</h1>
             <ul class="list-card" id="recommendedReads">
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 18rem;"
+                    v-for="(book, index) in books.slice(6, 10)"
+                    v-bind:item="book"
+                    v-bind:index="index"
+                    v-bind:key="book.id"
+                >
                   <a href="#">
-                      <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/0293-1/7B4/127/DC/%7B7B4127DC-3A42-419F-8010-32A18729833C%7DImg400.jpg" alt="Card image cap">
+                      <img class="card-img-top"
+                        :src="book.image"
+                        alt="Card image cap"
+                        style="height: 266.66px"
+                      >
                       <div class="card-body">
-                        <h3 class="card-title">Card title</h3>
-                        <p class="card-date">20-9-2021</p>
-                      </div>
-                  </a>
-                </div>
-                <div class="card" style="width: 18rem;">
-                  <a href="#">
-                      <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/0111-1/B08/57D/86/%7BB0857D86-EF18-4C49-AC52-B5E08796F5F9%7DImg400.jpg" alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title">Card title</h3>
-                        <p class="card-date">20-9-2021</p>
-                      </div>
-                  </a>
-                </div>
-                <div class="card" style="width: 18rem;">
-                  <a href="#">
-                      <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/2390-1/464/AB3/BC/%7B464AB3BC-A697-4979-B680-F693C4C793CF%7DImg400.jpg" alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title">Card title</h3>
-                        <p class="card-date">20-9-2021</p>
-                      </div>
-                  </a>
-                </div>
-                <div class="card" style="width: 18rem;">
-                  <a href="#">
-                      <img class="card-img-top" src="https://img1.od-cdn.com/ImageType-400/0111-1/B8A/A91/71/%7BB8AA9171-DA87-42D0-B906-D28E4413739E%7DImg400.jpg" alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title">Card title</h3>
-                        <p class="card-date">20-9-2021</p>
+                        <h3 class="card-title">{{ book.title }}</h3>
+                        <p class="card-date">{{ book.date }}</p>
                       </div>
                   </a>
                 </div>
@@ -182,9 +94,25 @@
 </template>
 
 <script>
-    export default {
-        name: 'header'
+import BookController from '../../controllers/book.controller'
+
+export default {
+    name: 'header',
+    data() {
+        return {
+            books: ''
+        }
+    },
+    methods: {
+        async getBooks() {
+            const data = await BookController.getBooks();
+            this.books = data.data;
+        }
+    },
+    mounted() {
+        this.getBooks();
     }
+}
 </script>
 
 <style>
--- frontend/src/components/body/Bodyv1.vue
+++ frontend/src/components/body/Bodyv1.vue
@@ -1,26 +1,34 @@
 <template>
-    <div class="container">
-        <h1 class="p-3 text-center">All The Books</h1>
-        <ul class="list-card" v-if="books">
-            <div class="card" style="width: 18rem;"
-                v-for="(book, index) in books"
-                v-bind:item="book"
-                v-bind:index="index"
-                v-bind:key="book.id"
-            >
-              <a href="#">
-                  <img class="card-img-top"
-                    :src="book.image"
-                    alt="Card image cap"
-                    style="height: 266.66px"
-                  >
-                  <div class="card-body">
-                    <h3 class="card-title">{{ book.title }}</h3>
-                    <p class="card-date">{{ book.date }}</p>
-                  </div>
-              </a>
-            </div>
-        </ul>
+    <div class="body container">
+        <div class="page-loader2" v-if="!books">
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+            <div class="cube2"></div>
+        </div>
+        <div class="content" v-if="books">
+            <h1 class="p-3 text-center">All The Books</h1>
+            <ul class="list-card">
+                <div class="card" style="width: 18rem;"
+                    v-for="(book, index) in books"
+                    v-bind:item="book"
+                    v-bind:index="index"
+                    v-bind:key="book.id"
+                >
+                  <a href="#">
+                      <img class="card-img-top"
+                        :src="book.image"
+                        alt="Card image cap"
+                        style="height: 266.66px"
+                      >
+                      <div class="card-body">
+                        <h3 class="card-title">{{ book.title }}</h3>
+                        <p class="card-date">{{ book.date }}</p>
+                      </div>
+                  </a>
+                </div>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -31,7 +39,7 @@ export default {
     name: 'bodyv1',
     data() {
         return {
-            books: ''
+            books: '',
         }
     },
     methods: {
